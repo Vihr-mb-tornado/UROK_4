de3f83e6ca9d21c47ebafc07c8385ea4f74f56f2
Laptop <yevhen003@gmail.com>
2024-09-29 11:00:00 +0200

    init

---
+++ Character.py
@@ -0,0 +1,13 @@
+﻿from character import Character
+
+playerl = Character('Vasya', 100, 3, 0)
+
+print(f'Створено нового персонажа: {playerl.name}')
+# print(f'У {player1.name} {player1.health} здоров\'я.')
+player1.show_stats()
+
+incoming_damage = 20
+print(f'Ha {player1.name} напали бандити і нанесли {incoming_damage} шкоди.')
+player1.health -= incoming_damage
+pftnt(f'Тепер у {player1.name} {player1.health} здоров\'я.')
+player1.show_stats()
